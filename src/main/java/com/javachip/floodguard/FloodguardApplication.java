776f582f31ac04445322fc0461b1c04cfc0e2c1a
Creeper0809 <creeper0809@hotmail.com>
2023-08-03 12:11:04 +0900

    asd

---
+++ src/main/java/com/javachip/floodguard/FloodguardApplication.java
@@ -0,0 +1,13 @@
+package com.javachip.floodguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javachip/floodguard/FloodguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javachip.floodguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
