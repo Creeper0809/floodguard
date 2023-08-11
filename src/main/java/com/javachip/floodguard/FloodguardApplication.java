8b8ab9d12ad2c276e2e185f386f0a28d29856919
Creeper0809 <creeper0809@hotmail.com>
2023-08-11 18:15:10 +0900

    cctv api 객체 리스트로 반환

--- src/main/java/com/javachip/floodguard/FloodguardApplication.java
+++ src/main/java/com/javachip/floodguard/FloodguardApplication.java
@@ -1,13 +1,28 @@
 package com.javachip.floodguard;
 
+import com.javachip.floodguard.api.CCTVApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@EnableScheduling
 public class FloodguardApplication {
-
+	@Autowired
+	private final CCTVApi cctv;
 	public static void main(String[] args) {
 		SpringApplication.run(FloodguardApplication.class, args);
+	}
+	@Scheduled(fixedRate = 1000)
+	public void test() {
+		var result = cctv.getCCTV("126.800000","34.900000","127.890000","35.100000");
+		for(var i : result){
+			System.out.println(i.name);
+		}
 	}
 
 }
